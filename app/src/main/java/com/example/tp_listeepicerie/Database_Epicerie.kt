75744974bc634bcc9fb7e570cd888eea5f9fd959
Jimmy Tran <2247081@bdeb.qc.ca>
2024-10-18 17:01:13 -0400

    Create Database_Epicerie.kt

---
+++ app/src/main/java/com/example/tp_listeepicerie/Database_Epicerie.kt
@@ -0,0 +1,31 @@
+package com.example.tp_listeepicerie
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Table_Epicerie::class], version = 1, exportSchema = false)
+abstract class Database_Epicerie: RoomDatabase() {
+    abstract fun epicerieDao(): EpicerieDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: Database_Epicerie? = null
+
+        fun getDatabase(ctx: Context): Database_Epicerie {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) return tempInstance
+
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    ctx.applicationContext,
+                    Database_Epicerie::class.java,
+                    "epicerie_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
