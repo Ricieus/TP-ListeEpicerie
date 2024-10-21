fdfc41188870fe9dfd0160dfda188868e8f563d0
Jimmy Tran <2247081@bdeb.qc.ca>
2024-10-20 20:50:28 -0400

    Filtrer si la BD existe déjà

--- app/src/main/java/com/example/tp_listeepicerie/MainActivity.kt
+++ app/src/main/java/com/example/tp_listeepicerie/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.tp_listeepicerie
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
@@ -65,25 +67,27 @@ class MainActivity : AppCompatActivity() {
         )
 
         val database = Database_Epicerie.getDatabase(applicationContext)
-        lifecycleScope.launch(Dispatchers.IO) {
-            for (epicerie in genericList) {
-                val existingItem = database.epicerieDao().findByName(epicerie.nom)
+        // https://stackoverflow.com/questions/3386667/query-if-android-database-exists
+        if ((applicationContext.getDatabasePath("epicerie_database")).exists()) {
+            lifecycleScope.launch(Dispatchers.IO) {
+                for (epicerie in genericList) {
+                    val existingItem = database.epicerieDao().findByName(epicerie.nom)
 
-                if (existingItem == null) {
-                    database.epicerieDao().insertEpicerie(epicerie)
+                    if (existingItem == null) {
+                        database.epicerieDao().insertEpicerie(epicerie)
+                    }
+                }
+
+                listEpicerie = database.epicerieDao().getAll()
+                cartItems = database.epicerieDao().getAllPanier()
+                launch(Dispatchers.Main) {
+                    recyclerView.adapter = ItemAdaptor(applicationContext, this@MainActivity, listEpicerie)
+                    recyclerViewCart.adapter = PanierAdaptor(applicationContext, this@MainActivity, cartItems)
                 }
             }
-
-
-
-            listEpicerie = database.epicerieDao().getAll()
-            cartItems = database.epicerieDao().getAllPanier()
-            launch(Dispatchers.Main) {
-                recyclerView.adapter = ItemAdaptor(applicationContext, this@MainActivity, listEpicerie)
-                recyclerViewCart.adapter = PanierAdaptor(applicationContext, this@MainActivity, cartItems)
-            }
         }
-//        val dbName = "epicerie_database"  // Your database name
+
+//        val dbName = "epicerie_database"
 //        applicationContext.deleteDatabase(dbName)
     }
 
