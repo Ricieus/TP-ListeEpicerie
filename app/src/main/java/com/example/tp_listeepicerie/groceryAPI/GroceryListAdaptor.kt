c5c4dd0c00b8fa9c5fb87bcb03855ad20112c940
manbir27 <2235096@bdeb.qc.ca>
2024-11-25 21:32:43 -0500

    essayer fragment pour retrofit api

---
+++ app/src/main/java/com/example/tp_listeepicerie/groceryAPI/GroceryListAdaptor.kt
@@ -0,0 +1,28 @@
+package com.example.tp_listeepicerie.groceryAPI
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tp_listeepicerie.R
+
+class GroceryListAdaptor(
+    val ctx: Context,
+    var groceryList: List<GroceryItem>
+): RecyclerView.Adapter<GroceryAPIHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GroceryAPIHolder {
+        val view = LayoutInflater.from(ctx).inflate(R.layout.grocery_view_holder, parent, false)
+        return GroceryAPIHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return groceryList.size
+    }
+
+    override fun onBindViewHolder(holder: GroceryAPIHolder, position: Int) {
+        val grocery = groceryList[position]
+        holder.name.text = grocery.name
+        holder.location.text = grocery.categoryId
+
+    }
+}
